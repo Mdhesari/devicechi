68013d10f8225bbb66b71f634fdf09026971bef0
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-10-23 04:23:00 +0330

    create phone related tables for ads

---
+++ database/migrations/2020_10_23_005115_create_phone_variants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhoneVariantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_variants', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('model_id');
+            $table->string('ram', 8);
+            $table->string('storage', 8);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone_variants');
+    }
+}
