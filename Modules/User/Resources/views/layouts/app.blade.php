24673049d5178a091d71adfcf8bc03a3e3f79dd6
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-10-23 22:18:03 +0330

    get rid of unneccessary public files

--- Modules/User/Resources/views/layouts/app.blade.php
+++ Modules/User/Resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/user/normalize.css') }}">
     <link rel="stylesheet" href="{{ mix('css/user/bootstrap-vue.css') }}">
     <link rel="stylesheet" href="{{ mix('css/user/bootstrap-rtl.css') }}">
 
