aa27c8e5242d0ba2ea8b638e2df3025167c3bd61
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-10-23 04:27:56 +0330

    add countries and cities table

---
+++ database/migrations/2020_10_23_005342_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 32);
+            $table->string('capital', 32);
+            $table->string('continent_code', 16);
+            $table->string('currency', 8);
+            $table->string('timezone', 32)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
---
+++ database/migrations/2020_10_23_005507_create_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 16);
+            $table->string('state', 16);
+            $table->foreignId('country_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
