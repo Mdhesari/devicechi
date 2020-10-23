32664a8970a84e0ffdb509c6207c3c8251e30e7e
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-10-23 06:20:41 +0330

    domain config for api route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,8 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::domain(config('app.url'))
+                ->prefix('api')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
