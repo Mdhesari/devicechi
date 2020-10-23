475b06f23262c24c7b0ea2461d77f215253879fb
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-10-23 14:03:06 +0330

    relocate all css, js, sass assets of Team to its desired module

--- Modules/Team/webpack.mix.js
+++ Modules/Team/webpack.mix.js
@@ -1,13 +1,19 @@
-const dotenvExpand = require('dotenv-expand');
-dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
+const dotenvExpand = require("dotenv-expand");
+dotenvExpand(
+    require("dotenv").config({ path: "../../.env" /*, debug: true*/ })
+);
 
-const mix = require('laravel-mix');
-require('laravel-mix-merge-manifest');
+const mix = require("laravel-mix");
+require("laravel-mix-merge-manifest");
 
-mix.setPublicPath('../../public').mergeManifest();
+mix.setPublicPath("../../public").mergeManifest();
 
-mix.js(__dirname + '/Resources/assets/js/app.js', 'js/team.js')
-    .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/team.css');
+mix.js(__dirname + "/Resources/assets/js/app.js", "js/team/team.js")
+    .sass(__dirname + "/Resources/assets/sass/app.scss", "css/team/team.css")
+    .postCss(__dirname + "/Resources/assets/css/tailwind.css", "css/team/tailwind.css", [
+        require("postcss-import"),
+        require("tailwindcss")
+    ]);
 
 if (mix.inProduction()) {
     mix.version();
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/tailwind.css",
-    "public/css",
-    [require("postcss-import"), require("tailwindcss")]
-);
+mix.js("resources/js/app.js", "public/js");
 
 // additonal custom resources
 mix.postCss("resources/css/normalize.css", "public/css/normalize.min.css");
