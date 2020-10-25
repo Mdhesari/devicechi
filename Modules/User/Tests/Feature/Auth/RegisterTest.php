1bb71588df14e7a898fe4079d0eca01d8cb691e4
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-10-25 23:34:06 +0330

    validation register tests

---
+++ Modules/User/Tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Modules\User\Tests\Feature\Auth;
+
+use DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Validation\ValidationException;
+
+class RegisterTest extends TestCase
+{
+
+    public function test_if_can_register()
+    {
+
+        $number = $this->generatePhoneNumber();
+
+        $response = $this->post(route('user.auth'), [
+            'phone' => $number,
+        ]);
+
+        $response->assertJson(['status' => 'ok']);
+    }
+
+    public function test_ensure_user_is_created_after_registeration()
+    {
+
+        $number = $this->generatePhoneNumber();
+
+        $user = DB::table('users')->where('phone', $number)->first();
+
+        $this->assertNull($user);
+
+        $this->post(route('user.auth'), [
+            'phone' => $number,
+        ]);
+
+        $user = DB::table('users')->where('phone', $number)->first();
+
+        $this->assertIsObject($user);
+    }
+
+    public function test_ensure_sessions_are_stored_after_registeration()
+    {
+
+        $number = $this->generatePhoneNumber();
+
+        $this->post(route('user.auth'), [
+            'phone' => $number,
+        ]);
+
+        $response = $this->get('/');
+
+        $response->assertSessionHas('phone');
+        $response->assertSessionHas('verification_code');
+    }
+
+    public function test_validation_fail_while_registering()
+    {
+
+        $this->expectException(ValidationException::class);
+
+        $number = '+989';
+
+        $response = $this->post(route('user.auth'), [
+            'phone' => $number,
+        ]);
+
+        $response->assertJsonValidationErrors(['phone']);
+    }
+
+    private function generatePhoneNumber()
+    {
+
+        return '+989' . rand(30, 40) . rand(0000000, 9999999);
+    }
+}
--- Modules/User/Tests/Feature/Auth/UserTest.php
+++ Modules/User/Tests/Feature/Auth/UserTest.php
@@ -20,32 +20,4 @@ class UserTest extends TestCase
         $response->assertJson(['status' => 'ok']);
     }
 
-    public function test_ensure_user_is_created_after_registeration()
-    {
-
-        $this->post(route('user.auth'), [
-            'phone' => '+989370038157',
-        ]);
-
-        $user = DB::table('users')->first();
-
-        $this->assertNull($user);
-
-        $user = DB::table('users')->first();
-
-        $this->assertIsObject($user);
-    }
-
-    public function test_ensure_sessions_are_stored_after_registeration()
-    {
-
-        $this->post(route('user.auth'), [
-            'phone' => '+989370038157',
-        ]);
-
-        $response = $this->get('/');
-
-        $response->assertSessionHas('phone');
-        $response->assertSessionHas('verification_code');
-    }
 }
