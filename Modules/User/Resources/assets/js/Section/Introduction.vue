f1fd39892092a2f7b310bfc5358443b6034060d7
Mohamad Fazel Hesari <>
2020-10-30 19:04:48 +0330

    translate home

--- Modules/User/Resources/assets/js/Section/Introduction.vue
+++ Modules/User/Resources/assets/js/Section/Introduction.vue
@@ -2,10 +2,11 @@
   <AppSection class="introduction">
     <b-container fluid="md">
       <div class="home-title">
-        <h1>موبایل خود را بصورت آنلاین بفروشید</h1>
+        <h1>
+          {{ $t.get("home.intro.title") }}
+        </h1>
         <h2 class="sub-title text-muted">
-          در سریعترین زمان ممکن موبایل کار کرده خود را به فروش برسانید و یا گوشی
-          کارکرده بخرید.
+          {{ $t.get("home.intro.desc") }}
         </h2>
       </div>
     </b-container>
