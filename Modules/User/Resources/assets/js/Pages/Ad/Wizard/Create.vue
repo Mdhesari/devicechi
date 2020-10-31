73a2e50b104d0bd9269ebf551ec385185f4736fe
Mohamad Fazel Hesari <>
2020-10-31 19:14:31 +0330

    setup Ad create wizard vue

---
+++ Modules/User/Resources/assets/js/Pages/Ad/Wizard/Create.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="create-ad">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: "step_1",
+    };
+  },
+};
+</script>
