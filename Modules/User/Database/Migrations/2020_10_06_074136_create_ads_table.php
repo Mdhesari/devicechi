0e6d21cfea89145dcdd8a8055e24a416b4a2b2b7
Mohamad Fazel Hesari <>
2020-10-31 19:27:58 +0330

    modify users table

--- Modules/User/Database/Migrations/2020_10_06_074136_create_ads_table.php
+++ Modules/User/Database/Migrations/2020_10_06_074136_create_ads_table.php
@@ -15,20 +15,20 @@ class CreateAdsTable extends Migration
     {
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
-            $table->foreignId('user_id');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->foreignId('user_id')->constrained();
             $table->foreignId('city_id')->nullable();
             $table->foreignId('model_id')->nullable();
             $table->foreignId('model_variant_id')->nullable();
-            $table->decimal('price', 12, 2);
+            $table->decimal('price', 12, 2)->nullable();
             $table->tinyInteger('age')->nullable(); // based on months
-            $table->unsignedTinyInteger('status')->default(1); /* 0: unavailable, 1: available, 2: pending */
+            $table->unsignedTinyInteger('status')->default(3); /* 0: unavailable, 1: available, 2: pending, 3: uncompleted */
             $table->text('location')->nullable();
             $table->json('meta_ad')->nullable();
             /* contacts, meta_title, meta_description, age  */
 
-            $table->boolean('is_multicard');
+            $table->boolean('is_multicard')->nullable();
             $table->timestamps();
         });
     }
