d086e8c998143c3e2524050324126d0f347a2c28
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-11-05 22:34:25 +0330

    brand item setup

--- Modules/User/Resources/assets/js/Pages/Ad/Wizard/ChooseBrand.vue
+++ Modules/User/Resources/assets/js/Pages/Ad/Wizard/ChooseBrand.vue
@@ -7,6 +7,39 @@
             <p class="form-desc">
                 {{ $t.get("ads.wizard.choose_brand.desc") }}
             </p>
+
+            <div class="row brand-list">
+                <div class="col-md-2 brand-item">
+                    <a href="#">
+                        <img
+                            src="https://cdn.recycledevice.com/brands/apple.png"
+                            alt="Apple"
+                        />
+                        <h4 class="brand-label">Apple</h4>
+                    </a>
+                </div>
+
+                <div class="col-md-2 brand-item">
+                    <a href="#">
+                        <img
+                            src="https://cdn.recycledevice.com/brands/asus.png"
+                            alt="Asus"
+                        />
+                        <h4 class="brand-label">Apple</h4>
+                    </a>
+                </div>
+
+                <div class="col-md-2 brand-item">
+                    <a href="#">
+                        <img
+                            src="https://cdn.recycledevice.com/brands/huawei.png"
+                            alt="Apple"
+                        />
+                        <h4 class="brand-label">Apple</h4>
+                    </a>
+                </div>
+            </div>
+
             <b-button v-if="isContinue" variant="secondary" type="submit">
                 {{ $t.get("ads.wizard.btn.continue") }}
             </b-button>
