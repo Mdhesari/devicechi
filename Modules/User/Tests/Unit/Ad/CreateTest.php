8c15e251f6866399e1c89e075d35dfe689b08291
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-11-05 23:59:52 +0330

    more create ad unit test

--- Modules/User/Tests/Unit/Ad/CreateTest.php
+++ Modules/User/Tests/Unit/Ad/CreateTest.php
@@ -10,16 +10,71 @@ use Modules\User\Entities\User;
 
 class CreateTest extends TestCase
 {
+
+    protected $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
     public function test_if_can_initialize()
     {
-
-        $user = User::factory()->create();
 
         $ad = new Ad;
         $ad->model_id = 1;
 
-        $user->ads()->save($ad);
+        $this->user->ads()->save($ad);
 
         $this->assertDatabaseCount('ads', 1);
+    }
+
+    public function test_if_can_continue_step()
+    {
+
+        $ad = new Ad;
+        $ad->model_id = 1;
+
+        $this->user->ads()->save($ad);
+
+        $ad = $this->user->ads()->whereStatus(3)->first();
+
+        $ad->model_variant_id = 1;
+        $ad->save();
+
+        $ad = $this->user->ads()->whereStatus(3)->first();
+
+        $this->assertNotNull($ad->model_variant_id);
+    }
+
+    public function test_if_can_finish_steps_go_to_pending_status()
+    {
+
+        $ad = new Ad;
+        $ad->model_id = 1;
+
+        $this->user->ads()->save($ad);
+
+        $ad = $this->user->ads()->whereStatus(3)->first();
+
+        $ad->model_variant_id = 1;
+        $ad->age = 30;
+        $ad->price = 7650000;
+        $ad->city_id = 1;
+        $ad->location = 'Iran, Tehran, Ekbatan phase 1';
+        $ad->title = 'فروش گوشی اپل 5s';
+        $ad->description = 'تازه خریدم و از اندروید بیشتر خوشم میاد.';
+        $ad->meta_ad = [
+            'title' => 'فروش گوشی موبایل',
+            'description' => 'راحت و فوری',
+        ];
+        $ad->status = 2;
+        $ad->save();
+
+        $ad->refresh();
+
+        $this->assertEquals(2, $ad->status);
     }
 }
