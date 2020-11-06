59206969f3c51bbd8bf45418037758d27d52a7e8
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-11-07 00:29:59 +0330

    clean up user routes

--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -10,11 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-use Goutte\Client;
-use Modules\User\Entities\PhoneBrand;
-use Modules\User\Entities\PhoneModel;
-use Modules\User\Http\Controllers\Home\HomeController;
-use Symfony\Component\DomCrawler\Link;
-use Symfony\Component\HttpClient\HttpClient;
-
