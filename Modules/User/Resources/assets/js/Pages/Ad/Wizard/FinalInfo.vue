9ff6cc8ebf77bff78d187886c185cea0c244c8c9
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-11-27 16:36:08 +0330

    update details step view

--- Modules/User/Resources/assets/js/Pages/Ad/Wizard/FinalInfo.vue
+++ Modules/User/Resources/assets/js/Pages/Ad/Wizard/FinalInfo.vue
@@ -7,6 +7,41 @@
             <p class="form-desc">
                 {{ __("ads.wizard.final_info.desc") }}
             </p>
+
+            <b-form-group
+                :label="__('ads.form.label.details.title')"
+                label-for="title"
+            >
+                <b-form-input
+                    type="text"
+                    id="title"
+                    required
+                    v-model="form.title"
+                    :placeholder="__('ads.form.placeholder.details.title')"
+                ></b-form-input>
+                <p class="text-danger" v-text="form.error('title')"></p>
+            </b-form-group>
+
+            <b-form-group
+                :label="__('ads.form.label.details.description')"
+                label-for="description"
+                class="ad-description-form"
+            >
+                <b-form-textarea
+                    id="description"
+                    required
+                    :placeholder="
+                        __('ads.form.placeholder.details.description')
+                    "
+                    rows="30"
+                    v-model="form.description"
+                ></b-form-textarea>
+                <p class="text-danger" v-text="form.error('description')"></p>
+            </b-form-group>
+
+            <b-button variant="secondary" @click.prevent="next">
+                {{ __("global.next") }}
+            </b-button>
         </form>
     </WizardStep>
 </template>
@@ -20,12 +55,15 @@ export default {
     },
     data() {
         return {
-            current_root: this.$inertia.page.props.current_root
+            form: this.$inertia.form({
+                title: null,
+                description: null
+            })
         };
     },
     methods: {
-        next(variant_id) {
-            // this.$emit("next");
+        next() {
+            this.form.post(route("user.ad.step_phone_details"));
         }
     }
 };
