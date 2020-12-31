863319a55d3382da8cfaf77cc7bc3c2f00f8fef6
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2020-12-31 12:25:54 +0330

    update lines

--- config/ghasedak.php
+++ config/ghasedak.php
@@ -4,9 +4,10 @@ return [
 
     'api_key' => env('GHASEDAK_API_KEY'),
 
-    'default_line' => env('GHASEDAK_DEFAULT_LINE', 'line_1'),
+    'default_line' => env('GHASEDAK_DEFAULT_LINE', 'private_1'),
 
     'lines' => [
+        'private_1' => '500057504101',
         'line_1' => '2000235',
         'line_2' => '210002100',
         'line_3' => '5000570',
