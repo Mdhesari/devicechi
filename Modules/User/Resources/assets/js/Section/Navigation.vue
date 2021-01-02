6e7c6f1575bc6b78b3f020dad5be25b9edc09f66
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-01-02 23:16:04 +0330

    update navigation

--- Modules/User/Resources/assets/js/Section/Navigation.vue
+++ Modules/User/Resources/assets/js/Section/Navigation.vue
@@ -3,8 +3,18 @@
   <nav class="main-menu" id="mainMenu">
     <!-- Navigation Links -->
     <ul>
-      <li><a href="#">آیتم اول</a></li>
-      <li><a href="#">آیتم دوم</a></li>
+      <li>
+      <inertia-link :href="route("user.ads")">صفحه اصلی</inertia-link>
+      </li>
+      <li>
+      <inertia-link href="#">تماس با ما</inertia-link>
+      </li>
+      <li>
+      <inertia-link href="#">قوانین</inertia-link>
+      </li>
+      <li>
+      <inertia-link href="#">درباره ما</inertia-link>
+      </li>
     </ul>
   </nav>
 </template>
