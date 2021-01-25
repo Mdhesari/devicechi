d2c7e7af0b2a38d9b9e52094c4ac222cba03629c
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-01-25 22:06:08 +0330

    update packages and fix production npm run

--- Modules/Team/webpack.mix.js
+++ Modules/Team/webpack.mix.js
@@ -10,10 +10,10 @@ mix.setPublicPath("../../public").mergeManifest();
 
 mix.js(__dirname + "/Resources/assets/js/app.js", "js/team/team.js")
     .sass(__dirname + "/Resources/assets/sass/app.scss", "css/team/team.css")
-    .postCss(__dirname + "/Resources/assets/css/tailwind.css", "css/team/tailwind.css", [
-        require("postcss-import"),
-        require("tailwindcss")
-    ]);
+    // .postCss(__dirname + "/Resources/assets/css/tailwind.css", "css/team/tailwind.css", [
+    //     require("postcss-import"),
+    //     require("tailwindcss")
+    // ]);
 
 if (mix.inProduction()) {
     mix.version();
