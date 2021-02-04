c3ae9fc2d4e2f3976b5dc3ce9bcb9e7324dc18ed
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-02-05 03:10:45 +0330

    get rid of unrequired jetstream files and actions

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\JetstreamServiceProvider::class,
         App\Providers\InertiaServiceProvider::class,
         App\Providers\SetupNotificationChannels::class
     ],
