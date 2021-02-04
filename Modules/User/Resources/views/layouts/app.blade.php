1f235ba72e6aa4b053a9b2b66ea0191a86013d43
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-02-05 02:23:26 +0330

    favicons

--- Modules/User/Resources/views/layouts/app.blade.php
+++ Modules/User/Resources/views/layouts/app.blade.php
@@ -14,6 +14,14 @@
     {!! Twitter::generate() !!}
     {!! JsonLd::generate() !!}
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('images/favicons/safari-pinned-tab.svg') }}" color="#ff6704">
+    <meta name="msapplication-TileColor" content="#ff6704">
+    <meta name="theme-color" content="#ff6704">
+
     <link rel="canonical" href="{{ config('app.name') }}" />
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/user/normalize.css') }}">
