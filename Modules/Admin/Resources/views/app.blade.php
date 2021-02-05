844106d316495fc8398851808ff00f2581dd27b6
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-02-05 22:49:01 +0330

    update admin app blade

--- Modules/Admin/Resources/views/app.blade.php
+++ Modules/Admin/Resources/views/app.blade.php
@@ -10,6 +10,14 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/favicons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('assets/favicons/safari-pinned-tab.svg') }}" color="#ff6704">
+    <meta name="msapplication-TileColor" content="#ff6704">
+    <meta name="theme-color" content="#ff6704">
+
     @styles
 
     @stack('add_styles')
