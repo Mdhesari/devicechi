8a6fec302af649802755b5c32d03b2eb5a5b0a20
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-03-06 15:22:52 +0330

    change routes and home page

--- Modules/User/Resources/assets/js/Pages/Home.vue
+++ Modules/User/Resources/assets/js/Pages/Home.vue
@@ -1,26 +1,424 @@
 <template>
-    <AuthLayout :user="user">
-        <IntroductionSection></IntroductionSection>
-        <HomeLoginSection></HomeLoginSection>
-        <HomeIntroSection></HomeIntroSection>
-    </AuthLayout>
+	<div>
+		<section class="banner-site">
+			<div class="container">
+				<div class="row">
+					<div class="h1holder">
+						<h1>دیواس چی مرجع تخصصی خرید و فروش گوشی</h1>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="search-bar-section py-5">
+			<div class="mini-container">
+				<div class="search-bar">
+					<input type="text" placeholder="دنبال چه گوشی می گردی " />
+					<button type="button" class="icon">
+						<img src="img/search.svg" alt="" />
+					</button>
+				</div>
+				<!-- <div class="filter-bar">
+                    <form action="#" class="filters">
+                         <div class="filter">
+                            <input type="checkbox" id="filter" name="" class="filter-input" >
+                            <label for="filter" class="filter-label">فیلتر 1</label>
+                         </div>
+                         <div class="filter">
+                            <input type="checkbox" id="filter" name="" class="filter-input" >
+                            <label for="filter" class="filter-label">فیلتر 1</label>
+                         </div>
+                         <div class="filter">
+                            <input type="checkbox" id="filter" name="" class="filter-input" >
+                            <label for="filter" class="filter-label">فیلتر 1</label>
+                         </div>
+                    </form>
+                </div> -->
+			</div>
+		</section>
+		<section class="step-section py-5">
+			<div class="container">
+				<div class="main-title">
+					<a href="#">
+						<h2>
+							گام های خرید و فروش
+						</h2>
+					</a>
+				</div>
+				<div class="row steps">
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 step">
+						<div class="inner">
+							<div class="icon">
+								<img src="img/get-price.png" alt="" />
+							</div>
+							<div class="title">
+								<h3>
+									گام شماره اول
+								</h3>
+							</div>
+							<div class="desc">
+								<p>
+									از آنجا که لورم ایپسوم، شباهت زیادی به متن های واقعی دارد،
+									طراحان معمولا از لورم ایپسوم استفاده میکنند
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 step">
+						<div class="inner">
+							<div class="icon">
+								<img src="img/book-pickup.png" alt="" />
+							</div>
+							<div class="title">
+								<h3>
+									گام شماره دوم
+								</h3>
+							</div>
+							<div class="desc">
+								<p>
+									از آنجا که لورم ایپسوم، شباهت زیادی به متن های واقعی دارد،
+									طراحان معمولا از لورم ایپسوم استفاده میکنند
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 step">
+						<div class="inner">
+							<div class="icon">
+								<img src="img/get-paid.png" alt="" />
+							</div>
+							<div class="title">
+								<h3>
+									گام شماره سوم
+								</h3>
+							</div>
+							<div class="desc">
+								<p>
+									از آنجا که لورم ایپسوم، شباهت زیادی به متن های واقعی دارد،
+									طراحان معمولا از لورم ایپسوم استفاده میکنند
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="normal-ads-section py-5">
+			<div class="container">
+				<div class="main-title">
+					<a href="#">
+						<h2>
+							آگهی ها
+						</h2>
+					</a>
+				</div>
+				<div class="row normal-ads">
+					<div class="col-md-3 col-xs-6 normal-ad">
+						<div class="inner">
+							<a href="#" class="fulllink"></a>
+							<div class="tumbnail">
+								<img
+									src="https://images.unsplash.com/photo-1574675904801-eb2cca16af12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1451&q=80"
+									alt=""
+								/>
+								<div class="bookmark">
+									<i class="fa-bookmark far" aria-hidden="true"></i>
+								</div>
+							</div>
+							<div class="details">
+								<div class="ad-name">
+									<h3>آیفون 6</h3>
+								</div>
+								<div class="ad-price">
+									<span>2000,000</span>
+									هزارتومان
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-xs-6 normal-ad">
+						<div class="inner">
+							<a href="#" class="fulllink"></a>
+							<div class="tumbnail">
+								<img
+									src="https://images.unsplash.com/photo-1574675904801-eb2cca16af12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1451&q=80"
+									alt=""
+								/>
+								<div class="bookmark">
+									<i class="fa-bookmark far" aria-hidden="true"></i>
+								</div>
+							</div>
+							<div class="details">
+								<div class="ad-name">
+									<h3>آیفون 6</h3>
+								</div>
+								<div class="ad-price">
+									<span>2000,000</span>
+									هزارتومان
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-xs-6 normal-ad">
+						<div class="inner">
+							<a href="#" class="fulllink"></a>
+							<div class="tumbnail">
+								<img
+									src="https://images.unsplash.com/photo-1574675904801-eb2cca16af12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1451&q=80"
+									alt=""
+								/>
+								<div class="bookmark">
+									<i class="fa-bookmark far" aria-hidden="true"></i>
+								</div>
+							</div>
+							<div class="details">
+								<div class="ad-name">
+									<h3>آیفون 6</h3>
+								</div>
+								<div class="ad-price">
+									<span>2000,000</span>
+									هزارتومان
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class=" col-md-3 col-xs-6 normal-ad">
+						<div class="inner">
+							<a href="#" class="fulllink"></a>
+							<div class="tumbnail">
+								<img
+									src="https://images.unsplash.com/photo-1574675904801-eb2cca16af12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1451&q=80"
+									alt=""
+								/>
+								<div class="bookmark">
+									<i class="fa-bookmark far" aria-hidden="true"></i>
+								</div>
+							</div>
+							<div class="details">
+								<div class="ad-name">
+									<h3>آیفون 6</h3>
+								</div>
+								<div class="ad-price">
+									<span>2000,000</span>
+									هزارتومان
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="more">
+						<a href="#">
+							مشاهده بیشتر
+							<i class="fa fa-arrow-left" aria-hidden="true"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="why-us-section py-5">
+			<div class="container">
+				<div class="main-title">
+					<a href="#">
+						<h2>
+							چرا ما ؟
+						</h2>
+					</a>
+				</div>
+				<div class="row why-us-boxes">
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 why-us-box">
+						<div class="inner">
+							<div class="right-layout">
+								<i class="fa fa-star" aria-hidden="true"></i>
+							</div>
+							<div class="left-layout">
+								<div class="title">
+									<h3>
+										محصولات خاص و با کیفیت
+									</h3>
+								</div>
+								<div class="desc">
+									<p>
+										اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید
+										به متن های برخورده اید
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 why-us-box">
+						<div class="inner">
+							<div class="right-layout">
+								<i class="fa fa-check" aria-hidden="true"></i>
+							</div>
+							<div class="left-layout">
+								<div class="title">
+									<h3>
+										خرید و فروش مطمئن
+									</h3>
+								</div>
+								<div class="desc">
+									<p>
+										اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید
+										به متن های برخورده اید
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 why-us-box">
+						<div class="inner">
+							<div class="right-layout">
+								<i class="fa fa-usd" aria-hidden="true"></i>
+							</div>
+							<div class="left-layout">
+								<div class="title">
+									<h3>
+										قیمت مناسب
+									</h3>
+								</div>
+								<div class="desc">
+									<p>
+										اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید
+										به متن های برخورده اید
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 why-us-box">
+						<div class="inner">
+							<div class="right-layout">
+								<i class="fa fa-thumbs-up" aria-hidden="true"></i>
+							</div>
+							<div class="left-layout">
+								<div class="title">
+									<h3>
+										سهولت در انتخاب
+									</h3>
+								</div>
+								<div class="desc">
+									<p>
+										اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید
+										به متن های برخورده اید
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 why-us-box">
+						<div class="inner">
+							<div class="right-layout">
+								<i class="fa fa-tag" aria-hidden="true"></i>
+							</div>
+							<div class="left-layout">
+								<div class="title">
+									<h3>
+										مشاوره رایگان
+									</h3>
+								</div>
+								<div class="desc">
+									<p>
+										اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید
+										به متن های برخورده اید
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 why-us-box">
+						<div class="inner">
+							<div class="right-layout">
+								<i class="fa fa-headphones" aria-hidden="true"></i>
+							</div>
+							<div class="left-layout">
+								<div class="title">
+									<h3>
+										پشتیبانی حرفه ای
+									</h3>
+								</div>
+								<div class="desc">
+									<p>
+										اگر شما یک طراح هستین و یا با طراحی های گرافیکی سروکار دارید
+										به متن های برخورده اید
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="brand-section py-5">
+			<div class="container">
+				<div class="main-title">
+					<a href="#">
+						<h2>
+							برند ها
+						</h2>
+					</a>
+				</div>
+				<div class="row brands">
+					<div class="swiper-container">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/apple.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/google.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/lenovo.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/mi.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/nokia.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/sum.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/nokia.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="inner-slider">
+									<a href="#">
+										<img src="img/brand/mi.jpg" alt="" />
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="swiper-pagination"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
-
-<script>
-import IntroductionSection from "./../Section/Introduction";
-import HomeLoginSection from "./../Section/HomeLogin";
-import HomeIntroSection from "./../Section/HomeIntro";
-import AuthLayout from "./../Layouts/FrontAuthLayout";
-import AppSection from "./../Components/AppSection";
-
-export default {
-    props: ["user"],
-    components: {
-        AuthLayout,
-        IntroductionSection,
-        AppSection,
-        HomeLoginSection,
-        HomeIntroSection
-    }
-};
-</script>
--- Modules/User/Routes/domain/web.php
+++ Modules/User/Routes/domain/web.php
@@ -10,13 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use Modules\User\Http\Controllers\Ad\AdAccessoryController;
+use Modules\User\Http\Controllers\Ad\AdAgeController;
+use Modules\User\Http\Controllers\Ad\AdContactController;
 use Modules\User\Http\Controllers\Ad\AdCreateController;
+use Modules\User\Http\Controllers\Ad\AdDemoController;
+use Modules\User\Http\Controllers\Ad\AdDetailsController;
+use Modules\User\Http\Controllers\Ad\AdHomeController;
+use Modules\User\Http\Controllers\Ad\AdLocationController;
 use Modules\User\Http\Controllers\Auth\LoginController;
 use Modules\User\Http\Controllers\Auth\SessionController;
 use Modules\User\Http\Controllers\Home\HomeController;
 use Modules\User\Http\Controllers\UserContactUsController;
 use Modules\User\Http\Controllers\UserController;
+
 
 Route::get('/contact-us', [UserContactUsController::class, 'index'])->name('contact-us');
 
@@ -37,4 +44,92 @@ Route::middleware('guest:sanctum')->name('user.')->group(function () {
     Route::middleware('english_numbers')->post('/auth', [SessionController::class, 'store'])->name('auth');
 
     Route::post('/auth/validate', [SessionController::class, 'verify'])->name('verify');
+});
+
+Route::prefix('/ads')->name('user.ad.')->group(function () {
+
+    Route::prefix('/sell/mobile')->group(function () {
+
+        Route::get('/{ad?}', [AdCreateController::class, 'show'])->name('create');
+
+        Route::get('/routes/steps/{step}', [BaseAdController::class, 'getStepRoute'])->name('routes');
+
+        Route::prefix('/process')->name('step_phone_')->group(function () {
+
+            Route::prefix('{ad}')->group(function () {
+
+                Route::get('/accessories', [AdAccessoryController::class, 'choose'])->name('accessories');
+
+                Route::post('/accessories', [AdAccessoryController::class, 'store']);
+
+                Route::get('/age', [AdAgeController::class, 'choose'])->name('age');
+
+                Route::post('/age', [AdAgeController::class, 'store']);
+
+                Route::get('/price', [AdPriceController::class, 'choose'])->name('price');
+
+                Route::middleware('english_numbers')->post('/price', [AdPriceController::class, 'store']);
+
+                Route::get('/pictures', [AdPictureController::class, 'choose'])->name('pictures');
+
+                Route::post('/pictures', [AdPictureController::class, 'store']);
+
+                Route::post('/pictures-upload', [AdPictureController::class, 'storeUploads'])->name('pictures_upload');
+
+                Route::delete('/pictures', [AdPictureController::class, 'delete']);
+
+                Route::get('/location', [AdLocationController::class, 'choose'])->name('location');
+
+                Route::post('/location', [AdLocationController::class, 'store']);
+
+                Route::get('/location/states/{city}', [AdLocationController::class, 'getState'])->name('location.states');
+
+                Route::get('/contact', [AdContactController::class, 'choose'])->name('contact');
+
+                Route::post('/contact', [AdContactController::class, 'store']);
+
+                Route::middleware('english_numbers')->post('/contact/add', [AdContactController::class, 'add'])->name('contact.add');
+
+                Route::delete('/contact/remove', [AdContactController::class, 'remove'])->name('contact.delete');
+
+                Route::middleware('english_numbers')->put('/contact/verify', [AdContactController::class, 'verify'])->name('contact.verify');
+
+                Route::get('/details', [AdDetailsController::class, 'choose'])->name('details');
+
+                Route::post('/details', [AdDetailsController::class, 'store']);
+
+                Route::get('/demo', [AdDemoController::class, 'show'])->name('demo');
+
+                Route::get('/{phone_model}/variants', [AdVariantController::class, 'choose'])->name('model_variant');
+
+                Route::post('/{phone_model}/variants', [AdVariantController::class, 'store']);
+            });
+
+            Route::put('/demo/publish', [AdDemoController::class, 'publish'])->name('demo.publish');
+
+            Route::put('/demo/delete', [AdDemoController::class, 'delete'])->name('demo.delete');
+
+            Route::put('/demo/archive', [AdDemoController::class, 'archive'])->name('demo.archive');
+
+            Route::get('/{phone_brand}/{ad?}', [AdModelController::class, 'choose'])->name('model');
+
+            Route::post('/{phone_brand}/{ad?}', [AdModelController::class, 'store']);
+        });
+    });
+
+    Route::get('/', [AdHomeController::class, 'index'])->name('home');
+
+    Route::get('/s', [AdHomeController::class, 'all'])->name('all');
+
+    Route::post('/s', [AdHomeController::class, 'search']);
+
+    Route::get('/get/brands', [AdMainController::class, 'getBrands'])->name('get.brands');
+
+    Route::get('/get/models', [AdMainController::class, 'getModels'])->name('get.models');
+
+    Route::get('/get/status/{status}', [AdMainController::class, 'getStatus'])->name('get.status');
+
+    Route::get('/{ad}', [AdHomeController::class, 'show'])->name('show');
+
+    Route::get('/p/{ad}', [AdHomeController::class, 'show'])->name('show.short-link');
 });
