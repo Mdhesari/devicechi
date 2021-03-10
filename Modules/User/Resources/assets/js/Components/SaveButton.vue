11943e1c662e11dddf9e12c088bb0047b5401db6
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-03-10 17:12:12 +0330

    only logged in users can bookmark ad

--- Modules/User/Resources/assets/js/Components/SaveButton.vue
+++ Modules/User/Resources/assets/js/Components/SaveButton.vue
@@ -1,37 +1,39 @@
 <template>
-    <div>
-        <button id="bookmark-btn" class="btn rounded" @click="save">
-            <b-icon
-                :icon="isSaved ? 'bookmark-check-fill' : 'bookmark'"
-            ></b-icon>
-        </button>
-        <b-tooltip target="bookmark-btn" triggers="click blur">
-            <span v-if="isSaved" v-text="__('global.bookmark')"></span>
+	<div>
+		<button id="bookmark-btn" class="btn rounded" @click="save">
+			<b-icon :icon="isSaved ? 'bookmark-check-fill' : 'bookmark'"></b-icon>
+		</button>
+		<b-tooltip target="bookmark-btn" triggers="click blur">
+			<span v-if="isSaved" v-text="__('global.bookmark')"></span>
 
-            <span v-if="!isSaved" v-text="__('global.unbookmark')"></span>
-        </b-tooltip>
-    </div>
+			<span v-if="!isSaved" v-text="__('global.unbookmark')"></span>
+		</b-tooltip>
+	</div>
 </template>
 
 <script>
 export default {
-    props: ["ad"],
-    data() {
-        return {
-            isSaved: Boolean(this.getProp("is_bookmarked_for_user"))
-        };
-    },
-    methods: {
-        async save() {
-            const response = await axios.post(route("user.ad.bookmark"), {
-                ad: this.ad.id,
-                attach: !this.isSaved
-            });
+	props: ['ad', 'user'],
+	data() {
+		return {
+			isSaved: Boolean(this.getProp('is_bookmarked_for_user'))
+		} 
+	},
+	methods: {
+		async save() {
+			if (!this.user) {
+				this.$inertia.visit(route('user.login'))
+			}
 
-            if (response.status == 200) {
-                this.isSaved = !this.isSaved;
-            }
-        }
-    }
-};
+			const response = await axios.post(route('user.ad.bookmark'), {
+				ad: this.ad.id,
+				attach: !this.isSaved
+			})
+
+			if (response.status == 200) {
+				this.isSaved = !this.isSaved
+			}
+		}
+	}
+}
 </script>
--- Modules/User/Resources/assets/js/Components/SingleAd.vue
+++ Modules/User/Resources/assets/js/Components/SingleAd.vue
@@ -1,53 +1,49 @@
 <template>
-    <section class="post-holder">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-6 single-post">
-                    <div class="post-inner">
-                        <div class="heading">
-                            <h1>
-                                {{ ad.title }}
-                            </h1>
-                        </div>
-                        <div class="publish-time">
-                            <span>
-                                {{ moment(ad.created_at).fromNow() }}
-                            </span>
-                            <span>در</span>
-                            <span v-text="ad.state.city.name"></span>
-                            <span>,</span>
-                            <span v-text="ad.state.name"></span>
-                        </div>
-                        <div class="actions my-4">
-                            <GetContactList :ad="ad"></GetContactList>
-                            <div class="btn-group btn-dock">
-                                <ShareButton
-                                    propId="copy-share-url"
-                                    :shareUrl="ad.short_url"
-                                ></ShareButton>
-                                <SaveButton :ad="ad"></SaveButton>
-                            </div>
-                        </div>
-                        <div class="specs">
-                            <ul
-                                class="list-group list-group-horizontal my-4 row accessories"
-                            >
-                                <li
-                                    v-for="(accessory, index) in ad.accessories"
-                                    :key="index"
-                                    class="list-group-item col bg-transparent border-0"
-                                >
-                                    <img
-                                        class="accessory-image"
-                                        v-b-tooltip
-                                        :title="
-                                            __('accessories.' + accessory.title)
-                                        "
-                                        :src="url(accessory.picture_path)"
-                                        :alt="accessory.title"
-                                    />
-                                </li>
-                                <!-- <li
+	<section class="post-holder">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-lg-6 single-post">
+					<div class="post-inner">
+						<div class="heading">
+							<h1>
+								{{ ad.title }}
+							</h1>
+						</div>
+						<div class="publish-time">
+							<span>
+								{{ moment(ad.created_at).fromNow() }}
+							</span>
+							<span>در</span>
+							<span v-text="ad.state.city.name"></span>
+							<span>,</span>
+							<span v-text="ad.state.name"></span>
+						</div>
+						<div class="actions my-4">
+							<GetContactList :ad="ad"></GetContactList>
+							<div class="btn-group btn-dock">
+								<ShareButton
+									propId="copy-share-url"
+									:shareUrl="ad.short_url"
+								></ShareButton>
+								<SaveButton :user="user" :ad="ad"></SaveButton>
+							</div>
+						</div>
+						<div class="specs">
+							<ul class="list-group list-group-horizontal my-4 row accessories">
+								<li
+									v-for="(accessory, index) in ad.accessories"
+									:key="index"
+									class="list-group-item col bg-transparent border-0"
+								>
+									<img
+										class="accessory-image"
+										v-b-tooltip
+										:title="__('accessories.' + accessory.title)"
+										:src="url(accessory.picture_path)"
+										:alt="accessory.title"
+									/>
+								</li>
+								<!-- <li
                                     v-for="(accessory, index) in accessories"
                                     :key="index"
                                     class="list-group-item bg-transparent border-0"
@@ -58,111 +54,116 @@
                                         :alt="accessory.title"
                                     />
                                 </li> -->
-                            </ul>
-                            <ul class="infos-phone">
-                                <li class="inner">
-                                    <div class="title">قیمت</div>
-                                    <div class="value">
-                                        {{ formatMoney(ad.price) }}
-                                        <span>تومان</span>
-                                    </div>
-                                </li>
-                                <!--
+							</ul>
+							<ul class="infos-phone">
+								<li class="inner">
+									<div class="title">قیمت</div>
+									<div class="value">
+										{{ formatMoney(ad.price) }}
+										<span>تومان</span>
+									</div>
+								</li>
+								<!--
                                 <li class="inner">
                                     <div class="title">رنگ</div>
                                     <div class="value">سیلور</div>
                                 </li>
                                 -->
 
-                                <li class="inner">
-                                    <div class="title">حافظه داخلی / رم</div>
-                                    <div class="value">
-                                        {{ printVariantInfo(ad.variant) }}
-                                    </div>
-                                </li>
-                                <li class="inner">
-                                    <div class="title">وضعیت</div>
-                                    <div class="value">موجود</div>
-                                </li>
-                                <li class="inner">
-                                    <div class="title">برند</div>
-                                    <div class="value">
-                                        {{
-                                            ad.phone_model.brand.persian_name +
-                                                " " +
-                                                ad.phone_model.brand.name
-                                        }}
-                                    </div>
-                                </li>
-                                <li class="inner">
-                                    <div class="title">مدل</div>
-                                    <div class="value">
-                                        {{ ad.phone_model.name }}
-                                    </div>
-                                </li>
-                                <li class="inner">
-                                    <div class="title">
-                                        پشتیبانی از چند سیمکارت
-                                    </div>
-                                    <div class="value">
-                                        {{ ad.is_multicard_read }}
-                                    </div>
-                                </li>
-                                <li class="inner">
-                                    <div class="title">قابل معاوضه</div>
-                                    <div class="value">
-                                        {{ ad.is_exchangeable_read }}
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="des">
-                            <h4>توضیحات</h4>
-                            <div v-html="adBody"></div>
-                        </div>
-                        <HamtaAlert class="my-4" />
-                    </div>
-                </div>
-                <div class="col-12 col-lg-6 gallery-post links">
-                    <AdPostGallery :pictures="ad.media" />
-                </div>
-            </div>
-        </div>
-    </section>
+								<li class="inner">
+									<div class="title">حافظه داخلی / رم</div>
+									<div class="value">
+										{{ printVariantInfo(ad.variant) }}
+									</div>
+								</li>
+								<li class="inner">
+									<div class="title">وضعیت</div>
+									<div class="value">موجود</div>
+								</li>
+								<li class="inner">
+									<div class="title">برند</div>
+									<div class="value">
+										{{
+											ad.phone_model.brand.persian_name +
+												' ' +
+												ad.phone_model.brand.name
+										}}
+									</div>
+								</li>
+								<li class="inner">
+									<div class="title">مدل</div>
+									<div class="value">
+										{{ ad.phone_model.name }}
+									</div>
+								</li>
+								<li class="inner">
+									<div class="title">
+										پشتیبانی از چند سیمکارت
+									</div>
+									<div class="value">
+										{{ ad.is_multicard_read }}
+									</div>
+								</li>
+								<li class="inner">
+									<div class="title">قابل معاوضه</div>
+									<div class="value">
+										{{ ad.is_exchangeable_read }}
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="des">
+							<h4>توضیحات</h4>
+							<div v-html="adBody"></div>
+						</div>
+						<HamtaAlert class="my-4" />
+					</div>
+				</div>
+				<div class="col-12 col-lg-6 gallery-post links">
+					<AdPostGallery :pictures="ad.media" />
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-import AdPostGallery from "./AdPostGallery";
-import HamtaAlert from "../Section/HamtaAlert";
-import GetContactList from "../Section/ContactList";
-import ShareButton from "../Components/ShareButton";
-import SaveButton from "../Components/SaveButton";
+import AdPostGallery from './AdPostGallery'
+import HamtaAlert from '../Section/HamtaAlert'
+import GetContactList from '../Section/ContactList'
+import ShareButton from '../Components/ShareButton'
+import SaveButton from '../Components/SaveButton'
 export default {
-    props: {
-        ad: {
-            type: Object
-        },
-        accessories: {
-            type: Array,
-            default: []
-        }
-    },
-    components: {
-        AdPostGallery,
-        HamtaAlert,
-        GetContactList,
-        ShareButton,
-        SaveButton
-    },
-    computed: {
-        adBody() {
-            return this.ad.description.replace(/(?:\r\n|\r|\n)/g, "<br>");
-        }
-    },
-    methods: {
-        printVariantInfo(variant) {
-            return variant.storage + " / " + variant.ram;
-        }
-    }
-};
+	props: {
+		ad: {
+			type: Object
+		},
+		accessories: {
+			type: Array,
+			default: []
+		}
+	},
+	data() {
+		return {
+			user: this.getProp('user')
+		}
+	},
+	components: {
+		AdPostGallery,
+		HamtaAlert,
+		GetContactList,
+		ShareButton,
+		SaveButton
+	},
+	computed: {
+		adBody() {
+			return this.ad.description.replace(/(?:\r\n|\r|\n)/g, '<br>')
+		}
+	},
+	methods: {
+		printVariantInfo(variant) {
+			return variant.storage + ' / ' + variant.ram
+		}
+	}
+}
 </script>
