56f73a5c2f51b7b174f588772b981026a821b67a
Mohamad Fazel <mdhesari99@gmail.com>
2021-04-05 15:05:31 +0430

    add version to app config

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'version' => '1.80.0',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
