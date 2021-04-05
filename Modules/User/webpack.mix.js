738671d9a9005a2fb8c3e32294ac137bc9a364d3
Mohamad Fazel <mdhesari99@gmail.com>
2021-04-05 14:50:11 +0430

    update docker compose db config to pgsql and fix styles

--- Modules/User/webpack.mix.js
+++ Modules/User/webpack.mix.js
@@ -10,7 +10,7 @@ mix.setPublicPath('../../public').mergeManifest()
 
 // mix.browserSync('localhost');
 
-mix.options({ uglify: { uglifyOptions: { compress: { drop_console: true } } } })
+// mix.options({ uglify: { uglifyOptions: { compress: { drop_console: true } } } })
 
 mix
 	.js(__dirname + '/Resources/assets/js/user.js', 'js/user/user.js')
