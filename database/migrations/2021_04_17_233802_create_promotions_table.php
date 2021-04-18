e973815d869cb98a88dae8dde8dcdca3bbf067f8
Mohamad Fazel <mdhesari99@gmail.com>
2021-04-18 05:52:35 +0430

    setup promotins table and model seeder

---
+++ database/migrations/2021_04_17_233802_create_promotions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', 12);
+            $table->string('currency', 8)->default('IRT');
+            $table->timestamp('activate_after_minutes')->nullable();
+            $table->boolean('one_time')->default(true);
+            $table->foreignId('admin_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promotions');
+    }
+}
