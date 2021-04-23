684ba8a0eebcaa5a1916a7f2b01469c431c00d6b
Mohamad Fazel <mdhesari99@gmail.com>
2021-04-23 23:30:04 +0430

    update home url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/ads';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
