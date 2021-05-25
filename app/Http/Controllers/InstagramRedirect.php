bc77a9337bff06c443f1190dc028f0ab56d51e3c
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-05-26 02:09:02 +0430

    more tests insta

--- app/Http/Controllers/InstagramRedirect.php
+++ app/Http/Controllers/InstagramRedirect.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Storage;
 
 class InstagramRedirect extends Controller
 {
@@ -16,13 +17,14 @@ class InstagramRedirect extends Controller
     {
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
         if (strpos($userAgent, 'Instagram')) {
-            $file = \fopen('instagram.txt', 'wr');
+            $file = \fopen($path = public_path('instagram.txt'), 'wr');
+            fwrite($file, "lorem ipsum");
+            fclose($file);
             header('Content-type: application/pdf');
             header('Content-Disposition: inline; filename= blablabla');
             header('Content-Transfer-Encoding: binary');
             header('Accept-Ranges: bytes');
-            @readfile($file);
-            fclose($file);
+            return @readfile($path);
         } else {
             return redirect()->route('user.home');
         }
